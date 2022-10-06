db4586aeb2653eff957806d48f4920852634a49b
Jasmin Dedic <jdedic1999@gmail.com>
2022-10-06 15:15:28 +0200

    Created starter files

---
+++ templates/header.js
@@ -0,0 +1,15 @@
+// Get header element 
+let header = document.querySelector(".header");
+
+
+// Function to generate header content
+function generateHeader() {
+    header.innerHTML = createHeaderContent();
+}
+
+// Function to create and return header content
+function createHeaderContent() {
+    return `
+        
+    `;
+}
