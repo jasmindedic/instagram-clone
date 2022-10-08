410e585ef10677a24db526e7b0eebc2c062405ce
Jasmin Dedic <jdedic1999@gmail.com>
2022-10-08 15:13:13 +0200

    Finished post and follow section

---
+++ data.js
@@ -0,0 +1,43 @@
+// Create needed arrays
+let profileImages = [
+    "./images/profile-pic1.jpg",
+    "./images/profile-pic2.jpg",
+    "./images/profile-pic3.jpg",
+    "./images/profile-pic4.jpg",
+    "./images/profile-pic5.jpg"
+];
+
+let names = [
+    "Ben_Kaik",
+    "Antonio_Quadio",
+    "Anne_marie",
+    "Franzi_19",
+    "Franzi_19"
+];
+
+let followerNames = [
+    "Lisa_979",
+    "Hansi_1001",
+    "Michael_li",
+    "Marcus_An",
+    "Herbert_1090"
+];
+
+let locations = [
+    "London",
+    "Berlin",
+    "Amsterdam",
+    "New York",
+    "Rom"
+];
+
+let images = [
+    "./images/image1.jpg",
+    "./images/image2.jpg",
+    "./images/image3.jpg",
+    "./images/image4.jpg",
+    "./images/image5.jpg"
+];
+let likesAmount = [76, 32, 93, 87, 90];
+
+let comments = [];
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Global variables
 let contentContainer = document.querySelector(".content");
 let mainContent = document.querySelector(".mainContent");
+let topProfileBar = document.querySelector(".topProfileBar");
 let sidebar = document.querySelector(".sidebar");
 
 
@@ -12,29 +13,111 @@ function init() {
 
 // Function to render functions that create html content
 function render() {
-    mainContent.innerHTML += generateTopProfileBar();
+    topProfileBar.innerHTML += generateTopProfileBar();
+    generatePosts();
+    generateSidebar();
 }
 
 // Function to generate top profile bar
 function generateTopProfileBar() {
     return /* html */`
-        <div class="topProfileBar">
             <div class="topProfileContainer">
-                <img class="profileImg" src="./images/profile-pic2.jpg">
+                <img class="profileImg topBarImg" src="./images/profile-pic2.jpg">
                 <p>Ben_Kaik</p>
             </div>
             <div class="topProfileContainer">
-                <img class="profileImg" src="./images/profile-pic3.jpg">
+                <img class="profileImg topBarImg" src="./images/profile-pic3.jpg">
                 <p>Antonio_Quadio</p>
             </div>
             <div class="topProfileContainer">
-                <img class="profileImg" src="./images/profile-pic4.jpg">
+                <img class="profileImg topBarImg" src="./images/profile-pic4.jpg">
                 <p>Anne_marie</p>
             </div>
             <div class="topProfileContainer">
-                <img class="profileImg" src="./images/profile-pic5.jpg">
+                <img class="profileImg topBarImg" src="./images/profile-pic5.jpg">
                 <p>Franzi_19</p>
             </div>
+    `;
+}
+
+// Function to generate posts (invoking createPostContent function)
+function generatePosts() {
+    for (let i = 0; i < names.length; i++) {
+
+        let postContainer = document.querySelector(".postContainer");
+        // Get right index directly into variable
+        let profileImage = profileImages[i];
+        let name = names[i];
+        let location = locations[i];
+        let image = images[i];
+        let likeAmount = likesAmount[i];
+        let comment = comments[i];
+
+
+        postContainer.innerHTML += createPostContent(profileImage, name, location, image, likeAmount, comment);
+    }
+
+}
+
+// Function to create return content for post
+function createPostContent(profileImage, name, location, image, likeAmount, comment) {
+    return /* html */`
+        <div class="post">
+            <div class="profileRow">
+                <div class="userInfo">  
+                <img class="profileImg" src="${profileImage}">
+                    <div class="nameLocation">
+                        <p>${name}</p>
+                        <p class="location">${location}</p>
+                    </div>
+                    </div>
+                    <i class="fa-solid fa-ellipsis"></i>
+            </div>
+            <div class="imagePostContainer">
+                <img class="imagePost" src="${image}">
+            </div>
+            <div class="threeBtnContainer">
+                <i class="fa-solid fa-heart threeBtn"></i>
+                <i class="fa-solid fa-comment threeBtn"></i>
+                <i class="fa-solid fa-location-arrow threeBtn"></i>
+            </div>
+            <div class="likesAmount">
+                <p>Gefällt ${likeAmount} Mal</p>
+            </div>
+                <hr>
+            <div class="commentArea">
+                <i class="fa-solid fa-face-smile"></i>
+                <input type="text" placeholder="Type your comment">
+                <button class="postBtn">Posten</button>
+            </div>
+        </div>
+    `;
+}
+
+// Function to generate sidebar (invoking createSidebarContent() function)
+function generateSidebar() {
+    for (let i = 0; i < followerNames.length; i++) {
+        let sidebarContent = document.querySelector(".sidebarContent");
+
+        // Get right index directly into variable
+        let profileImage = profileImages[i];
+        let name = names[i];
+        let followerName = followerNames[i];
+
+        sidebarContent.innerHTML += createSidebarContent(profileImage, name, followerName);
+    }
+}
+
+// Function to create and return sidebar content
+function createSidebarContent(profileImage, name, followerName) {
+    return /* html */`
+        <div class="followExample">
+            <img class="profileImg" src="${profileImage}">
+            <div class="followNames">
+                <p>${name}</p>
+                <p>${followerName} ist follower</p>
+            </div>
+            <p class="follow">Folgen</p>
         </div>
     `;
 }
